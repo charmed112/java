f2bcc50be92c58c34acb866f5598b6341c6c756a
charmed <cutqqq@mail.ru>
2023-02-05 21:50:47 +0500

    комит2

---
+++ src/Lesson_6.java
@@ -0,0 +1,18 @@
+public class Lesson_6 {
+    public static final String QUERY = "select * from students where ";
+    public static final String PARAMS = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"} ";
+    public static void main(String[] args) {
+        String paramToStr = PARAMS.replace('{', ' ').replace('}', ' ');
+        String[] newStr = paramToStr.split(",");
+        StringBuilder result = new StringBuilder(QUERY);
+        for (int i = 0; i < newStr.length; i++) {
+            String[] resultItem = newStr[i].replace('"', ' ').split(":");
+            if (!"null".equals(resultItem[1].trim())) {
+                result.append(resultItem[0].trim()).append(" = ").append('"').append(resultItem[1].trim()).append('"'); /*ключ и значение соединили через = для вывода */
+                if (i < newStr.length - 2)
+                    result.append(" and ");
+            }
+        }
+        System.out.println(result);
+    }
+}
