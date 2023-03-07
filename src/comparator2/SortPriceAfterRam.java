47d6935af8a123da96df898cdf8f14a3513a7cc7
charmed <cutqqq@mail.ru>
2023-03-08 00:41:08 +0500

    комит13

--- src/comparator2/SortPriceAfterRam.java
+++ src/comparator2/SortPriceAfterRam.java
@@ -6,20 +6,17 @@ public class SortPriceAfterRam  implements Comparator<Notebook> {
     @Override
     public int compare(Notebook notebook1, Notebook notebook2) {
 
-        if (notebook1.getRam() < notebook2.getRam()) {
-            return -1;
-        }
-        if (notebook1.getRam() > notebook2.getRam()) {
-            return 1;
-        }
         if (notebook1.getRam() == notebook2.getRam()) {
+        if (notebook1.getPrice() == notebook2.getPrice()) {
             return 0;
         }
         if (notebook1.getPrice() < notebook2.getPrice()) {
             return -1;
         }
-        if (notebook1.getPrice() == notebook2.getPrice()) {
-            return 0;
+            return 1;
+        }
+        if (notebook1.getRam() < notebook2.getRam()) {
+            return -1;
         }
         return 1;
     }
