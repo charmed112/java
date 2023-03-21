627ecaa18e6f55d9518b01fdf743f13ca3d41818
charmed <cutqqq@mail.ru>
2023-03-21 21:00:48 +0500

    комит16

---
+++ src/lesson6/Robot.java
@@ -0,0 +1,8 @@
+package lesson6;
+
+public interface Robot {
+    public void changeDirection(Direction direction);
+    public void move();
+    public long getId();
+    public Point getPoint();
+}
